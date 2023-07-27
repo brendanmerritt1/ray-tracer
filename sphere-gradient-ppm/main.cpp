6205d092abe58664cec4e5a60db9253b3cfff132
Brendan Merritt <brendanmerritt1@gmail.com>
2023-07-27 01:54:50 -0400

    Adding a ray-traced sphere to the image, shading its colors with its surface normals

--- sphere-gradient-ppm/main.cpp
+++ sphere-gradient-ppm/main.cpp
@@ -4,10 +4,29 @@
 
 #include <iostream>
 
-// Function that returns a gradient background color.
+// Function that will determine if a ray P(t) will hit the sphere.
+double hitSphere(const point3 &center, double radius, const ray &r) {
+    vec3 origin_to_center = r.origin() - center;             // Vector from center C to point P = (P-C). Full expanded equation: t^2*b*b + t*2*b*(A-C) + (A-C)*(A-C)-r^2 = 0
+    auto a = r.direction().len_sq();                         // a = b*b. Vector dotted with itself is equal to square length of that vector.
+    auto b = 2.0 * dotProd(origin_to_center, r.direction()); // b = 2*b*(A-C)
+    auto c = origin_to_center.len_sq() - radius * radius;    // c = (A-C)*(A-C)-r^2
+    auto discriminant = b * b - 4.0 * a * c;                   // Discriminant determines how many roots exist. If d > 0, two real solutions. If d = 0, one real solution.
+    if (discriminant < 0) {
+        return -1.0; // If no real solutions, t = -1.
+    } else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a); // If at least one real solution, 0 <= t <= 1.
+    }
+}
+
+// Function that calculates the color of a pixel based on the direction of a ray.
 color rayColor(const ray &r) {
+    auto t = hitSphere(point3(0, 0, -1), 0.5, r); // Determine the hit point of the ray on the sphere, if it exists.
+    if (t > 0.0) {
+        vec3 norm_vec = unitVec(r.rayFunc(t) - vec3(0, 0, -1));                   // Use the calculated hit point to compute a normalized vector (perpendicular to surface).
+        return 0.5 * color(norm_vec.x() + 1, norm_vec.y() + 1, norm_vec.z() + 1); // Mapping x/y/z to r/g/b.
+    }
     vec3 unit_dir = unitVec(r.direction());                             // Translates ray into a normalized unit vector.
-    auto t = 0.5 * (unit_dir.y() + 1.0);                                // -1.0 < y < 1.0 after normalization. Therefore 0 < t < 1.0.
+    t = 0.5 * (unit_dir.y() + 1.0);                                     // -1.0 < y < 1.0 after normalization. Therefore 0.0 < t < 1.0.
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0); // Linear interpolation: blended = (1-t)*startVal + t*endVal
 }
 
@@ -34,9 +53,9 @@ int main() {
     for (int i = img_height - 1; i >= 0; --i) {
         std::cerr << "\rRows remaining to scan: " << i << ' ' << std::flush;
         for (int j = 0; j < img_width; ++j) {
-            auto u = double(j) / (img_width - 1);
-            auto v = double(i) / (img_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            auto u = double(j) / (img_width - 1);  // Vector horizontal component
+            auto v = double(i) / (img_height - 1); // Vector vertical component
+            ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = rayColor(r);
             writeColor(std::cout, pixel_color);
         }
