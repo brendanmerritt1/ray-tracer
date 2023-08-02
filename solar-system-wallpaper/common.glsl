75ce994dc1445545f69ca97fe5118e663fe3eef6
Brendan Merritt <brendanmerritt1@gmail.com>
2023-08-01 23:39:09 -0400

    Added texture to planets, stars, animation

--- solar-system-wallpaper/common.glsl
+++ solar-system-wallpaper/common.glsl
@@ -1,15 +1,118 @@
-/**
-    Common types and functions used for ray tracing.
-*/
-
 #ifndef COMMON_GLSL
 #define COMMON_GLSL
 
-// Utility Functions
+/**
+    Constants
+**/
+const float PI = 3.14159265359;
+const float PHI = 1.61803398875;
+
+/**
+    Structs
+**/
+struct Ray {
+    vec3 o;     // origin
+    vec3 d;     // direction (set with normalized vector)
+    float t;    // time
+};
+
+struct HitRecord {
+    vec3 pos;
+    vec3 normal;
+    float t;    // Ray parameter
+};
+
+struct Sphere {
+    vec3 center;
+    float radius;
+};
+
+/**
+    Ray Functions
+**/
+Ray createRay(vec3 o, vec3 d, float t) {
+    Ray r;
+    r.o = o;
+    r.d = d;
+    r.t = t;
+    return r;
+}
+
+Ray createRay(vec3 o, vec3 d) {
+    return createRay(o, d, 0.0);
+}
+
+vec3 pointOnRay(Ray r, float t) {
+    return r.o + r.d * t;
+}
+
+/**
+    Sphere Functions
+**/
+Sphere createSphere(vec3 center, float radius) {
+    Sphere s;
+    s.center = center;
+    s.radius = radius;
+    return s;
+}
+
+bool hitSphere(Sphere s, Ray r, float t_min, float t_max, out HitRecord rec) {
+    vec3 origin_center = r.o - s.center;
+    float a = dot(r.d, r.d);
+    float b = 2.0 * dot(origin_center, r.d);
+    float c = dot(origin_center, origin_center) - s.radius * s.radius;
+    float discriminant = b * b - 4.0 * a * c;
+
+    if(discriminant > 0.0) {
+        float sqrt_discriminant = sqrt(discriminant);
+        float root = (-b - sqrt_discriminant) / (2.0 * a);
+        if(root < t_max && root > t_min) {
+            rec.t = root;
+            rec.pos = pointOnRay(r, rec.t);
+            rec.normal = (rec.pos - s.center) / s.radius;
+            return true;
+        }
+        root = (-b + sqrt_discriminant) / (2.0 * a);
+        if(root < t_max && root > t_min) {
+            rec.t = root;
+            rec.pos = pointOnRay(r, rec.t);
+            rec.normal = (rec.pos - s.center) / s.radius;
+            return true;
+        }
+    }
+    return false;
+}
+
+vec2 sphIntersect(in vec3 ro, in vec3 rd, in vec3 center, float radius) {
+    vec3 origin_center = ro - center;
+    float b = dot(origin_center, rd);
+    float c = dot(origin_center, origin_center) - radius * radius;
+    float discriminant = b * b - c;
+    if(discriminant < 0.0)
+        return vec2(-1.0);
+    discriminant = sqrt(discriminant);
+    return vec2(-b - discriminant, -b + discriminant);
+}
+
+/**
+    Utility Functions - Credit to Inigo Quilez for providing many of these functions
+**/
+vec3 RotateY(vec3 pos, float angle) {
+    return vec3(pos.x * cos(angle) - pos.z * sin(angle), pos.y, pos.x * sin(angle) + pos.z * cos(angle));
+}
+
+vec3 RotateX(vec3 pos, float angle) {
+    return vec3(pos.x, pos.y * cos(angle) - pos.z * sin(angle), pos.y * sin(angle) + pos.z * cos(angle));
+}
+
 float getHash(vec3 p) {
     p = fract(p * 0.3183099 + .1);
     p *= 17.0;
     return fract(p.x * p.y * p.z * (p.x + p.y + p.z));
+} // Hash function created by Inigo Quilez
+
+float getHashFloat(float p) {
+    return fract(sin(p) * 158.5453123);
 } // Hash function created by Inigo Quilez
 
 float getNoise(in vec3 x) {
@@ -23,6 +126,7 @@ float getNoise(in vec3 x) {
 float getFBM(vec3 p) {
     float noise = 0.0;
 
+    // Domain warping - using fBm to warp the space of a fBm (analogy: dream in a dream)
     vec3 warp = vec3(getNoise(p * 0.8 + vec3(13.0, 44.0, 15.0)), getNoise(p * 0.8 + vec3(43.0, 74.0, 25.0)), getNoise(p * 0.8 + vec3(33.0, 14.0, 75.0)));
 
     warp -= vec3(0.5);
@@ -39,22 +143,96 @@ float getFBM(vec3 p) {
         getNoise(p * 96.25) * 0.00125;
 
     return noise;
-} // Fractal Brownian Motion function created by Inigo Quilez
+} // Fractal Brownian Motion function created by Inigo Quilez. A simple sum of noise waves with increasing frequencies and decreasing amplitudes.
 
-vec3 toLinear(vec3 c) {
-    return pow(c, vec3(2.2));
+float getFBMClouds(vec3 p) {
+    float noise = 0.0;
+    // Domain warping
+    vec3 warp = vec3(getNoise(p * 0.8 + vec3(13.0, 44.0, 15.0)), getNoise(p * 0.8 + vec3(43.0, 74.0, 25.0)), getNoise(p * 0.8 + vec3(33.0, 14.0, 75.0)));
+
+    warp -= vec3(0.5);
+
+    p += vec3(123.0, 234.0, 55.0);
+    p += warp * 0.2;
+
+    noise = getNoise(p) * 1.0 +
+        getNoise(p * 5.02) * 0.49 +
+        getNoise(p * 11.11) * 0.24 +
+        getNoise(p * 23.05) * 0.12 +
+        getNoise(p * 45.05) * 0.055;
+    return noise;
+} // Fractal Brownian Motion function created by Inigo Quilez.
+
+vec2 inverseSF(vec3 p, float n) {
+    float m = 1.0 - 1.0 / n;
+
+    float phi = min(atan(p.y, p.x), PI), cosTheta = p.z;
+
+    float k = max(2.0, floor(log(n * PI * sqrt(5.0) * (1.0 - cosTheta * cosTheta)) / log(PHI + 1.0)));
+    float Fk = pow(PHI, k) / sqrt(5.0);
+    vec2 F = vec2(round(Fk), round(Fk * PHI));
+
+    vec2 ka = 2.0 * F / n;
+    vec2 kb = 2.0 * PI * (fract((F + 1.0) * PHI) - (PHI - 1.0));
+
+    mat2 iB = mat2(ka.y, -ka.x, kb.y, -kb.x) / (ka.y * kb.x - ka.x * kb.y);
+
+    vec2 c = floor(iB * vec2(phi, cosTheta - m));
+    float d = 8.0;
+    float j = 0.0;
+    for(int s = 0; s < 4; s++) {
+        vec2 uv = vec2(float(s - 2 * (s / 2)), float(s / 2));
+
+        float i = dot(F, uv + c);
+
+        float phi = 2.0 * PI * fract(i * PHI);
+        float cosTheta = m - 2.0 * i / n;
+        float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+
+        vec3 q = vec3(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
+        float squaredDistance = dot(q - p, q - p);
+        if(squaredDistance < d) {
+            d = squaredDistance;
+            j = i;
+        }
+    }
+    return vec2(j, sqrt(d));
+} // Inverse Spherical Fibonacci points created by Inigo Quilez.
+
+float getSceneDistance(vec3 ro, vec3 rd, vec3 center, float radius, float MAX_DIST) {
+    vec2 intersect = sphIntersect(ro, rd, center, radius);
+    if(intersect.x >= 0.0) {
+        return intersect.x;
+    } else {
+        return MAX_DIST;
+    }
 }
 
-vec3 toGamma(vec3 c) {
-    return pow(c, vec3(1.0 / 2.2));
+float getLight(vec3 LIGHT_POS, vec3 PLANET_CENTER, float PLANET_RADIUS, float SURF_DIST, float MAX_DIST, vec3 p, vec3 n, float height, float waterMask) {
+    vec3 light_pos = LIGHT_POS;
+    vec3 l = normalize(light_pos - p);
+
+    // Computes the normal with two height samples (only computing one extra here)
+    vec3 planet_bisect = normalize(p + l * 0.01 - PLANET_CENTER) * 0.5 * PLANET_RADIUS;
+    planet_bisect = RotateY(planet_bisect, iTime * 0.05);
+    float height_bisect = getFBM(planet_bisect) * 0.6;
+    float deltaH = height_bisect - height;
+
+    n = normalize(n - deltaH * l * 40.0 * (1.0 - waterMask));
+
+    float diffuse = dot(n, l); // Used for lights at night
+    float d = getSceneDistance(p + n * SURF_DIST * 2.0, l, PLANET_CENTER, PLANET_RADIUS, MAX_DIST);
+    if(d < length(light_pos - p))
+        diffuse *= 0.1;
+
+    return diffuse;
 }
 
-vec3 RotateY(vec3 pos, float angle) {
-    return vec3(pos.x * cos(angle) - pos.z * sin(angle), pos.y, pos.x * sin(angle) + pos.z * cos(angle));
-}
+float Density(vec3 pos, vec3 PLANET_CENTER, float PLANET_RADIUS, float ATMOS_THICK, float ATMOS_DENS) {
+    float distance_to_center = length(pos - PLANET_CENTER);
+    float relative_pos = clamp((distance_to_center - PLANET_RADIUS) / ATMOS_THICK, 0.0, 1.0);
 
-vec3 RotateX(vec3 pos, float angle) {
-    return vec3(pos.x, pos.y * cos(angle) - pos.z * sin(angle), pos.y * sin(angle) + pos.z * cos(angle));
+    return ATMOS_DENS * exp(-relative_pos);
 }
 
 #endif
--- solar-system-wallpaper/main.glsl
+++ solar-system-wallpaper/main.glsl
@@ -1,25 +1,25 @@
 #include "./common.glsl"
-#include "./helperFunc.glsl"
 
 #iChannel0 "self"
+#iChannel1 "https://stionary.sirv.com/Images/noise.png"
 
 // Initial Conditions
 #define MAX_STEPS 100.0
-#define MAX_DIST 100.0
+#define MAX_DIST 10000.0
 #define SURF_DIST 0.001
 #define OCEANS_LEVEL 0.60
-#define PLANET_CENTER vec3(-4, -3, 25)
+#define PLANET_CENTER vec3(-3, -3, 25)
 #define PLANET_RADIUS 7.0
-#define MOON_CENTER vec3(54, -50, 145)
+#define MOON_CENTER vec3(53, -50, 145)
 #define MOON_RADIUS 7.0
-#define ATMOS_THICK 0.5
+#define ATMOS_THICK 0.25
 #define ATMOS_DENS 2.0
-#define LIGHT_POS vec3(20 * 3, 15 * 3, 10)
+#define LIGHT_POS vec3(40, 25, -5)
 
 int hitSurface(Ray r, float t_min, float t_max, out HitRecord rec) {
     // 0 = planet
     // 1 = moon
-    // 2 = atmosphere
+    // 2 = space
     int hit;
     rec.t = t_max;
 
@@ -34,42 +34,151 @@ int hitSurface(Ray r, float t_min, float t_max, out HitRecord rec) {
     return hit;
 }
 
-vec3 rayTrace(Ray r) {
+vec3 rayTrace(Ray r, vec4 noise) {
     HitRecord rec;
     vec3 color = vec3(0, 0, 0);
-    if(hitSurface(r, 0.001, 10000.0, rec) == 0) {
+    vec2 atmos = sphIntersect(r.o, r.d, PLANET_CENTER, ATMOS_THICK + PLANET_RADIUS);
+    float dist = getSceneDistance(r.o, r.d, PLANET_CENTER, PLANET_RADIUS, MAX_DIST);
+    int hit = hitSurface(r, SURF_DIST, MAX_DIST, rec);
+
+    if(hit == 0) {
         // Ray hits planet
+        atmos.y = dist;
         vec3 pos = rec.pos;
         vec3 norm = rec.normal;
-
         vec3 local_pos = (pos - PLANET_CENTER) * 0.5;
         local_pos = RotateY(local_pos, iTime * 0.05);
 
-        float heat_distribution = smoothstep(3.5, 0.25, (abs(local_pos.y) + getNoise(local_pos + vec3(-150, 65, 10))));
-        float height = getFBM(local_pos) * 0.45;
-        float water = smoothstep(OCEANS_LEVEL + 0.05, OCEANS_LEVEL - 0.05, height);
+        // Water noise
+        float heat_distribution = smoothstep(4.5, 0.25, (abs(local_pos.y) + getNoise(local_pos + vec3(-20, 55, 20))));
+        float height = getFBM(local_pos) * 0.50;
+        float water = smoothstep(OCEANS_LEVEL + 0.02, OCEANS_LEVEL - 0.05, height);
         float waterHeight = clamp((height / OCEANS_LEVEL), 0.0, 1.1);
-        float foamNoise = getNoise(local_pos*5.0*(1.5+0.25*sin(iTime*0.1)))*0.25;
-        float foamLimit = smoothstep(0.6+foamNoise, 1.0, waterHeight);
 
+        // Shallow water
+        float foamNoise = getNoise(local_pos * 7.0 * (0.75 + 0.25 * sin(iTime * 0.1))) * 0.25;
+        float foamLimit = smoothstep(0.65 + foamNoise, 1.0, waterHeight);
+        foamLimit = 0.7 * foamLimit + 0.3 * fract(foamLimit * 4.0 - iTime * 0.2) * foamLimit * smoothstep(1.0, 0.85, foamLimit);
+
+        // Combining various water shades
         vec3 warmWaterColor = mix(vec3(0.0, 0.2, 0.6), vec3(0.0, 0.4, 1.0), smoothstep(0.0, 1.0, waterHeight));
         vec3 coolWaterColor = mix(vec3(0.15, 0.3, 0.5), vec3(0.2, 0.5, 0.7), smoothstep(0.0, 1.0, waterHeight));
         vec3 waterColor = mix(coolWaterColor, warmWaterColor, heat_distribution);
         waterColor = mix(waterColor, vec3(0.7, 0.8, 0.9), foamLimit);
-        vec3 finalColor = waterColor;
-        color = finalColor;
-    } else if(hitSurface(r, 0.001, 10000.0, rec) == 1) {
+
+        // Ice generation
+        float iceDistribution = smoothstep(3.8, 4.3, foamLimit + smoothstep(0.7, 0.85, height) * 0.25 + (abs(local_pos.y) - water * 0.5 + getNoise(local_pos + vec3(-20, 55, 20))));
+        float wideIceDistribution = smoothstep(2.5, 3.0, foamLimit + smoothstep(0.7, 0.85, height) * 0.25 + (abs(local_pos.y) - water * 0.5 + height * 0.5));
+        float flatten = water * (1.0 - iceDistribution);
+        flatten = max(flatten, iceDistribution * 0.75);
+
+        // Land generation
+        float groundHeight = clamp(((height - OCEANS_LEVEL) / (1.0 - OCEANS_LEVEL)), 0.0, 1.0);
+        vec3 groundColor = mix(vec3(0.4, 0.1, 0.0), vec3(1.0, 0.5, 0.4), clamp(groundHeight * 3.0, 0.0, 1.0));
+
+        vec3 finalColor = mix(groundColor, waterColor, water);
+        finalColor = mix(finalColor, mix(vec3(1.1, 1.1, 1.1), vec3(0.9, 1.0, 1.0), abs(sin(height * 10.0))), iceDistribution);
+
+        // Lighting and shadows
+        float light = getLight(LIGHT_POS, PLANET_CENTER, PLANET_RADIUS, SURF_DIST, MAX_DIST, pos, norm, height, flatten);
+        float light_diffuse = clamp(light, 0.0, 1.0);
+        float inv_light_diffuse = smoothstep(0.1, 0.05, light);
+        vec3 light_pos = LIGHT_POS;
+        vec3 norm_light = normalize(light_pos - pos);
+        float rawNormDotL = clamp(dot(norm, norm_light), 0.0, 1.0);
+        light_diffuse = max(light_diffuse, rawNormDotL * 0.7);
+        light_diffuse *= rawNormDotL;
+        float fresnel = pow(1.0 - clamp(dot(norm, -r.d), 0.0, 1.0), 3.0);
+        inv_light_diffuse *= (1.0 - fresnel);
+        light_diffuse *= (1.0 - fresnel);
+
+        color = vec3(light_diffuse) * finalColor;
+
+        // Trees and cities
+        float citiesLimit = smoothstep(0.0, 0.1, groundHeight) * smoothstep(0.15, 0.13, groundHeight) * (1.0 - water) * (1.0 - iceDistribution);
+        float greenArea = smoothstep(0.0, 0.01, groundHeight) * smoothstep(0.45, 0.3, groundHeight) * (1.0 - water) * (1.0 - wideIceDistribution);
+        vec2 city = inverseSF(normalize(local_pos), 140.0) + inverseSF(normalize(local_pos), 100.0);
+        local_pos = normalize(local_pos);
+        float town = smoothstep(0.28, 0.1, city.y);
+
+        // Light intensity for cities
+        vec2 light_info1 = inverseSF(local_pos, 20000.0);
+        vec2 light_info2 = inverseSF(RotateX(local_pos, 1.0), 40000.0);
+        vec2 light_info3 = inverseSF(RotateX(local_pos, 1.5), 30000.0);
+        float light_intensity = min(min(light_info1.y * 1.25 * (1.0 + smoothstep(0.8, 0.4, getHashFloat((light_info1.x) * 0.02))), light_info2.y * 1.75 * (1.0 + smoothstep(0.8, 0.4, getHashFloat((light_info1.x) * 0.02)))), light_info3.y * (1.0 + smoothstep(0.8, 0.4, getHashFloat((light_info1.x) * 0.02))));
+        light_intensity = smoothstep(0.01, 0.0, light_intensity);
+
+        // Applying trees and cities to world
+        float cityArea = smoothstep(0.2, 0.18, city.y) * citiesLimit;
+        citiesLimit *= town * light_intensity;
+        greenArea *= smoothstep(0.4, 0.3, city.y) * smoothstep(0.25, 0.28, city.y);
+        color = mix(color, vec3(light_diffuse) * mix(vec3(0.45, 0.65, 0.15), vec3(0.1, 0.35, 0.1), getNoise(local_pos * 200.0)), clamp(greenArea - getNoise(local_pos * 100.0) * 0.5, 0.0, 1.0));
+        color = mix(color, vec3(0.15), cityArea);
+        color = mix(color, vec3(0.25), clamp(citiesLimit * 2.0, 0.0, 1.0));
+        color += vec3(0.95, 0.8, 0.5) * inv_light_diffuse * citiesLimit * 2.0;
+
+        // Clouds
+        vec3 local_pos_clouds = (pos - PLANET_CENTER) * 0.25;
+        local_pos_clouds = RotateY(local_pos_clouds, iTime * 0.05 + local_pos_clouds.y * 0.85);
+        float clouds = getFBMClouds(local_pos_clouds * 3.0 - vec3(iTime * 0.025)) * 0.6;
+        vec3 local_pos_clouds_bisect = (pos + norm_light * 0.1 - PLANET_CENTER) * 0.25;
+        local_pos_clouds_bisect = RotateY(local_pos_clouds_bisect, iTime * 0.05);
+        float clouds_bisect = getFBMClouds(local_pos_clouds_bisect * 3.0 - vec3(iTime * 0.025)) * 0.6;
+        float level = smoothstep(OCEANS_LEVEL - 0.1, OCEANS_LEVEL + 0.5, height);
+        clouds = smoothstep(0.5 + level * 0.85, 0.8 + level * 0.85, clouds);
+        float cloudShadow = 1.0 - smoothstep(0.5 + level * 0.85, 0.8 + level * 0.85, clouds_bisect) * 0.5 * (1.0 - clamp(dot(norm, norm_light), 0.0, 1.0));
+        color = mix(color * cloudShadow, vec3(1.0) * rawNormDotL * cloudShadow, clouds);
+
+    } else if(hit == 1) {
         // Ray hits moon
         vec3 pos = rec.pos;
         vec3 norm = rec.normal;
         vec3 light_pos = LIGHT_POS;
-        vec3 l = normalize(light_pos - pos);
-        float shadows = clamp(dot(l, norm), 0.0, 1.0);
+        vec3 norm_light = normalize(light_pos - pos);
+        float shadows = clamp(dot(norm_light, norm), 0.0, 1.0);
         float noise = getFBM(pos * 0.4);
         color = mix(vec3(0.2), vec3(0.4), noise) * shadows;
-    } else {
-        // Ray hits atmosphere
+    } else if(hit == 2) {
+        // Ray hits space
         color = vec3(0.06);
+        vec3 pos = normalize(r.o + r.d * 1000.0);
+        pos = pos.xyz;
+
+        // Stars
+        for(float i = 0.0; i < 5.0; ++i) {
+            vec2 info = inverseSF(pos, 60000.0 + i * 1000.0);
+            float random = getHashFloat((info.x + i * 20.0) * 0.01);
+            float dist_to_star = smoothstep(0.0002 + 0.001 * pow((1.0 - random), 30.0), 0.0002, info.y) * smoothstep(0.1, 0.0, random);
+            color = max(color, vec3(dist_to_star));
+        }
+
+        // Space noise (nebulae)
+        float noise = getFBM(pos * 2.0 + vec3(0.0, 0.0, iTime * 0.05));
+        float nebulae = smoothstep(0.4, 1.8, noise);
+        float nebulae1 = max(0.3 - abs(nebulae - 0.3), 0.0);
+        float nebulae2 = max(0.2 - abs(nebulae - 0.4), 0.0);
+        float nebulae3 = max(0.1 - abs(nebulae - 0.5), 0.0);
+        vec3 nebulaeColor = vec3(0.0, 0.2, 0.7) * nebulae1 + vec3(0.5, 0.4, 0.3) * nebulae2 + vec3(0.1, 0.2, 0.4) * nebulae3;
+        color += nebulaeColor;
+    }
+    if(atmos.x >= 0.0) {
+        // Ray hits atmosphere
+        atmos.x -= noise.r;
+        float atmos_remaining = atmos.y - atmos.x;
+        vec3 start_pos = r.o + r.d * atmos.x;
+        float light_energy = 0.0;
+        float transmittance = 1.0;
+        float steps = 0.005;
+        for(float i = 0.0; i < atmos_remaining; i += steps) {
+            vec3 pos = start_pos + r.d * i;
+            float loc_density = Density(pos, PLANET_CENTER, PLANET_RADIUS, ATMOS_THICK, ATMOS_DENS) * steps;
+            vec3 light_dir = -normalize(pos - LIGHT_POS);
+            float shadow = getSceneDistance(pos, light_dir, PLANET_CENTER, PLANET_RADIUS, MAX_DIST);
+            shadow = step(MAX_DIST * 0.9, shadow);
+            light_energy += loc_density * shadow;
+            transmittance *= (1.0 - loc_density * 1.0);
+        }
+        color = mix(color, mix(vec3(0.1, 0.15, 0.7), vec3(0.65, 0.8, 1.0), (1.0 - exp(-light_energy * 0.6))) * clamp(light_energy * 0.5, 0.0, 1.0), (1.0 - transmittance) * (1.0 - exp(-light_energy * 0.6)));
     }
     return color;
 }
@@ -78,11 +187,12 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv = (fragCoord - 0.5 * iResolution.xy) / iResolution.y;
     vec3 offset = vec3(sin(iTime * 0.1) * 3.0, 0.25 + (cos(iTime * 0.1) * .5), 0);
     offset = mix(offset, vec3(4.0 - iMouse.x / iResolution.x * 8.0, 4.0 - (iMouse.y / iResolution.y) * 8.0, 0), step(0.001, iMouse.z));
+    vec4 noise = texture(iChannel1, fragCoord / 1024.0);
 
     vec3 ro = offset + vec3(0.0, 12.0, 0.0);
     vec3 rd = normalize(vec3(uv.x, uv.y, 2));
     rd = RotateX(rd, 0.5);
     Ray r = createRay(ro, rd);
-    vec3 col = rayTrace(r);
+    vec3 col = rayTrace(r, noise);
     fragColor = vec4(col, 1.0);
 }
